8726507b5804313938fc83c6d30fb65ccbc5183c
Harikrishnan-MR <mrharikrishnan1216@gmail.com>
2025-11-29 21:58:53 +0530

    smooth scroll implemented in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -20,6 +20,14 @@ const LeetCodeIcon = () => (
 );
 
 const Footer = () => {
+
+  const handleScroll = (sectionId) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   const socialLinks = [
     { icon: <MdEmail />, link: `mailto:${import.meta.env.VITE_EMAIL}` },
     { icon: <FaPhone />, link: `tel:${import.meta.env.VITE_PHONE_NUMBER}` },
@@ -29,13 +37,31 @@ const Footer = () => {
   ];
 
   return (
-    <footer className="bg-black text-white py-8 px-[12vw] md:px-[7vw] lg:px-[20vw]">
+    <footer className="bg-[#0d081f] text-white py-8 px-[12vw] md:px-[7vw] lg:px-[20vw]">
       <div className="container mx-auto text-center">
 
         {/* Name */}
         <h2 className="text-xl font-semibold text-purple-500">
           Harikrishnan M R
         </h2>
+        {/* Navigation Links - Responsive */}
+        <nav className="flex flex-wrap justify-center space-x-4 sm:space-x-6 mt-4">
+          {[
+            { name: "About", id: "about" },
+            { name: "Skills", id: "skills" },
+            { name: "Experience", id: "experience" },
+            { name: "Projects", id: "projects" },
+            { name: "Education", id: "education" },
+          ].map((item, index) => (
+            <button
+              key={index}
+              onClick={() => handleScroll(item.id)}
+              className="hover:text-purple-500 text-sm sm:text-base my-1"
+            >
+              {item.name}
+            </button>
+          ))}
+        </nav>
 
         {/* Social Icons */}
         <div className="flex flex-wrap justify-center space-x-4 mt-6">
